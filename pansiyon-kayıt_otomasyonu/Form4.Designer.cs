5d051a8039dac8ea270974f7550228ea142a48ce
sinemgurbuz <sinemgurbuz50@hotmail.com>
2023-06-02 19:00:35 +0300

    form düzenlemelerini yaptım.

--- pansiyon-kayıt_otomasyonu/Form4.Designer.cs
+++ pansiyon-kayıt_otomasyonu/Form4.Designer.cs
@@ -98,7 +98,7 @@
             // 
             this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(203, 296);
+            this.button2.Location = new System.Drawing.Point(145, 313);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(169, 73);
             this.button2.TabIndex = 7;
